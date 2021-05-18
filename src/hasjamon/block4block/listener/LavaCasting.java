3da12d04e0b49e1b1efb2117475a5845f6ba88e9
BuildTools <unconfigured@null.spigotmc.org>
2021-05-18 13:57:05 +0700

    Fixed lavacasting (cobblestone -> andesite; obsidian, snow etc. forms normally).

--- src/hasjamon/block4block/listener/LavaCasting.java
+++ src/hasjamon/block4block/listener/LavaCasting.java
@@ -2,19 +2,19 @@ package hasjamon.block4block.listener;
 
 import org.bukkit.Material;
 import org.bukkit.block.Block;
-import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockFormEvent;
 
 public class LavaCasting implements Listener {
-    // Prevents lavacasting (i.e., combinations of lava and water that form cobblestone).
-    // Accidentally disables obsidian formation... Oh well.
+    // Modifies lavacasting to form andesite instead of cobblestone.
     @EventHandler
-    public void onCobbleFormation(BlockFormEvent e) {
+    public void onBlockForm(BlockFormEvent e) {
         Block b = e.getBlock();
 
-        b.setType(Material.ANDESITE);
-        e.setCancelled(true);
+        if(e.getNewState().getType() == Material.COBBLESTONE) {
+            b.setType(Material.ANDESITE);
+            e.setCancelled(true);
+        }
     }
 }
