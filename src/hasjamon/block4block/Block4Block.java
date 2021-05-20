51805f260f21e5e3b33773931d564f1e30acfd6d
BuildTools <unconfigured@null.spigotmc.org>
2021-05-20 09:56:58 +0700

    Added hints, a random one of which is broadcast to all players every 5 minutes.

--- src/hasjamon/block4block/Block4Block.java
+++ src/hasjamon/block4block/Block4Block.java
@@ -3,9 +3,12 @@ package hasjamon.block4block;
 import hasjamon.block4block.command.*;
 import hasjamon.block4block.listener.*;
 import hasjamon.block4block.files.ConfigManager;
+import org.bukkit.Bukkit;
 import org.bukkit.command.PluginCommand;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
+import java.util.List;
+import java.util.Random;
 
 public class Block4Block extends JavaPlugin{
     public PluginManager pluginManager = getServer().getPluginManager();
@@ -18,6 +21,20 @@ public class Block4Block extends JavaPlugin{
         cfg = new ConfigManager(); // Initializes config
         registerEvents(); // Registers all the listeners
         setCommandExecutors(); // Registers all the commands
+
+        // Show a hint every 5 minutes (20 ticks/second * 300 seconds)
+        getServer().getScheduler().scheduleSyncRepeatingTask(this, this::showHint, 0, 20 * 300);
+    }
+
+    private void showHint() {
+        List<?> hints = getConfig().getList("hints");
+
+        if(hints != null) {
+            Random rand = new Random();
+            String hint = (String) hints.get(rand.nextInt(hints.size()));
+
+            Bukkit.broadcastMessage(hint);
+        }
     }
 
     private void setCommandExecutors() {
