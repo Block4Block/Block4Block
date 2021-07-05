f8dc94644224e779a8d172b075454a0c6991d69f
Bahm <x.benjamin.ahm@gmail.com>
2021-07-05 17:18:55 +0700

    Moved specialTypes to config (now called b4b-substitutions).

--- src/hasjamon/block4block/utils/utils.java
+++ src/hasjamon/block4block/utils/utils.java
@@ -27,64 +27,6 @@ public class utils {
     public static int minSecBetweenAlerts;
     private static boolean masterBookChangeMsgSent = false;
 
-    static {
-        specialTypes.put(Material.REDSTONE_WIRE, Material.REDSTONE);
-        specialTypes.put(Material.WALL_TORCH, Material.TORCH);
-        specialTypes.put(Material.REDSTONE_WALL_TORCH, Material.REDSTONE_TORCH);
-        specialTypes.put(Material.TALL_SEAGRASS, Material.SEAGRASS);
-        specialTypes.put(Material.CHORUS_PLANT, Material.CHORUS_FRUIT);
-        specialTypes.put(Material.KELP_PLANT, Material.KELP);
-        specialTypes.put(Material.BAMBOO_SAPLING, Material.BAMBOO);
-        specialTypes.put(Material.WARPED_WALL_SIGN, Material.WARPED_SIGN);
-        specialTypes.put(Material.ACACIA_WALL_SIGN, Material.ACACIA_SIGN);
-        specialTypes.put(Material.BIRCH_WALL_SIGN, Material.BIRCH_SIGN);
-        specialTypes.put(Material.CRIMSON_WALL_SIGN, Material.CRIMSON_SIGN);
-        specialTypes.put(Material.JUNGLE_WALL_SIGN, Material.JUNGLE_SIGN);
-        specialTypes.put(Material.DARK_OAK_WALL_SIGN, Material.DARK_OAK_SIGN);
-        specialTypes.put(Material.OAK_WALL_SIGN, Material.OAK_SIGN);
-        specialTypes.put(Material.SPRUCE_WALL_SIGN, Material.SPRUCE_SIGN);
-        specialTypes.put(Material.SKELETON_WALL_SKULL, Material.SKELETON_SKULL);
-        specialTypes.put(Material.WITHER_SKELETON_WALL_SKULL, Material.WITHER_SKELETON_SKULL);
-        specialTypes.put(Material.ZOMBIE_WALL_HEAD, Material.ZOMBIE_HEAD);
-        specialTypes.put(Material.PLAYER_WALL_HEAD, Material.PLAYER_HEAD);
-        specialTypes.put(Material.CREEPER_WALL_HEAD, Material.CREEPER_HEAD);
-        specialTypes.put(Material.DRAGON_WALL_HEAD, Material.DRAGON_HEAD);
-        specialTypes.put(Material.BRAIN_CORAL_WALL_FAN, Material.BRAIN_CORAL_FAN);
-        specialTypes.put(Material.BUBBLE_CORAL_WALL_FAN, Material.BUBBLE_CORAL_FAN);
-        specialTypes.put(Material.DEAD_BRAIN_CORAL_WALL_FAN, Material.DEAD_BRAIN_CORAL_FAN);
-        specialTypes.put(Material.DEAD_BUBBLE_CORAL_WALL_FAN, Material.DEAD_BUBBLE_CORAL_FAN);
-        specialTypes.put(Material.DEAD_FIRE_CORAL_WALL_FAN, Material.DEAD_FIRE_CORAL_FAN);
-        specialTypes.put(Material.DEAD_HORN_CORAL_WALL_FAN, Material.DEAD_HORN_CORAL_FAN);
-        specialTypes.put(Material.DEAD_TUBE_CORAL_WALL_FAN, Material.DEAD_TUBE_CORAL_FAN);
-        specialTypes.put(Material.FIRE_CORAL_WALL_FAN, Material.FIRE_CORAL_FAN);
-        specialTypes.put(Material.HORN_CORAL_WALL_FAN, Material.HORN_CORAL_FAN);
-        specialTypes.put(Material.TUBE_CORAL_WALL_FAN, Material.TUBE_CORAL_FAN);
-        specialTypes.put(Material.BLACK_WALL_BANNER, Material.BLACK_BANNER);
-        specialTypes.put(Material.BLUE_WALL_BANNER, Material.BLUE_BANNER);
-        specialTypes.put(Material.BROWN_WALL_BANNER, Material.BROWN_BANNER);
-        specialTypes.put(Material.CYAN_WALL_BANNER, Material.CYAN_BANNER);
-        specialTypes.put(Material.GRAY_WALL_BANNER, Material.GRAY_BANNER);
-        specialTypes.put(Material.GREEN_WALL_BANNER, Material.GREEN_BANNER);
-        specialTypes.put(Material.LIGHT_BLUE_WALL_BANNER, Material.LIGHT_BLUE_BANNER);
-        specialTypes.put(Material.LIGHT_GRAY_WALL_BANNER, Material.LIGHT_GRAY_BANNER);
-        specialTypes.put(Material.LIME_WALL_BANNER, Material.LIME_BANNER);
-        specialTypes.put(Material.MAGENTA_WALL_BANNER, Material.MAGENTA_BANNER);
-        specialTypes.put(Material.ORANGE_WALL_BANNER, Material.ORANGE_BANNER);
-        specialTypes.put(Material.PINK_WALL_BANNER, Material.PINK_BANNER);
-        specialTypes.put(Material.PURPLE_WALL_BANNER, Material.PURPLE_BANNER);
-        specialTypes.put(Material.RED_WALL_BANNER, Material.RED_BANNER);
-        specialTypes.put(Material.WHITE_WALL_BANNER, Material.WHITE_BANNER);
-        specialTypes.put(Material.YELLOW_WALL_BANNER, Material.YELLOW_BANNER);
-        specialTypes.put(Material.TRAPPED_CHEST, Material.CHEST);
-        specialTypes.put(Material.SWEET_BERRY_BUSH, Material.SWEET_BERRIES);
-        specialTypes.put(Material.INFESTED_STONE, Material.STONE);
-        specialTypes.put(Material.INFESTED_COBBLESTONE, Material.COBBLESTONE);
-        specialTypes.put(Material.INFESTED_STONE_BRICKS, Material.STONE_BRICKS);
-        specialTypes.put(Material.INFESTED_CRACKED_STONE_BRICKS, Material.CRACKED_STONE_BRICKS);
-        specialTypes.put(Material.INFESTED_MOSSY_STONE_BRICKS, Material.MOSSY_STONE_BRICKS);
-        specialTypes.put(Material.INFESTED_CHISELED_STONE_BRICKS, Material.CHISELED_STONE_BRICKS);
-    }
-
     public static String chat(String message) {
         return ChatColor.translateAlternateColorCodes('&', message);
     }
@@ -385,8 +327,9 @@ public class utils {
         if(requiresBlock) {
             Material requiredType = b.getType();
 
-            if (specialTypes.containsKey(requiredType))
-                requiredType = specialTypes.get(requiredType);
+            ConfigurationSection substitutions = plugin.getConfig().getConfigurationSection("b4b-substitutions");
+            if (substitutions != null && substitutions.contains(requiredType.name()))
+                requiredType = Material.valueOf(substitutions.getString(requiredType.name()));
 
             if (p.getInventory().getItemInOffHand().getType() == requiredType) {
                 p.getInventory().getItemInOffHand().setAmount(p.getInventory().getItemInOffHand().getAmount() - 1);
