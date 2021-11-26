f113c2e06309ddde04f14600a5ade4becb9259d1
Bahm <x.benjamin.ahm@gmail.com>
2021-11-26 20:43:24 +0700

    /bed now tells you how many hours are left.

--- src/hasjamon/block4block/command/BedCommand.java
+++ src/hasjamon/block4block/command/BedCommand.java
@@ -2,6 +2,7 @@ package hasjamon.block4block.command;
 
 import hasjamon.block4block.Block4Block;
 import hasjamon.block4block.events.BedCmdSucceededEvent;
+import hasjamon.block4block.utils.utils;
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.command.Command;
@@ -11,6 +12,8 @@ import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 
+import java.text.DecimalFormat;
+
 public class BedCommand implements CommandExecutor {
     private final Block4Block plugin;
 
@@ -19,23 +22,28 @@ public class BedCommand implements CommandExecutor {
     }
 
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args){
-        if(sender instanceof Player){
-            Player p = (Player) sender;
-            String pID = p.getUniqueId().toString();
+        if(sender instanceof Player player){
+            String pID = player.getUniqueId().toString();
             FileConfiguration bedCommandUsage = plugin.cfg.getBedCommandUsage();
             long now = System.nanoTime();
+            long nextAvailable = bedCommandUsage.getLong(pID, 0);
+            long diff = now - nextAvailable;
 
-            if(now > bedCommandUsage.getLong(pID, 0)){
-                p.getInventory().addItem(new ItemStack(Material.WHITE_BED, 1));
-                p.sendMessage(ChatColor.GRAY + "A bed has been added to your inventory!");
+            if(now > nextAvailable){
+                player.getInventory().addItem(new ItemStack(Material.WHITE_BED, 1));
+                player.sendMessage(ChatColor.GRAY + "A bed has been added to your inventory!");
 
                 bedCommandUsage.set(pID, now + 24 * 60 * 60 * 1e9);
                 plugin.cfg.saveBedCommandUsage();
             }else{
-                p.sendMessage(ChatColor.GRAY + "It's been less than 24 hours since your last use of /bed!");
+                DecimalFormat decimals = new DecimalFormat("#.#");
+                String hours = decimals.format(diff / (60 * 60 * 1e9));
+                String msg = utils.chat("&7It's been less than 24 hours since your last use of /bed! Try again in " + hours + " hours.");
+
+                player.sendMessage(msg);
             }
 
-            plugin.pluginManager.callEvent(new BedCmdSucceededEvent(p));
+            plugin.pluginManager.callEvent(new BedCmdSucceededEvent(player));
             return true;
         }
         return false;
