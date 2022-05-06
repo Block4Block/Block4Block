Block4Block/Block4Block

commit 48efade0311a7ecd80a0afef27988d92a7dfbc67

Fixed bug with books with interacting with AIR.

src/main/java/hasjamon/block4block/listener/BookEdit.java
@@ -28,11 +28,54 @@ public class BookEdit implements Listener {
         this.plugin = plugin;
     }
 
+    @EventHandler
+    public void onOpenBook(PlayerInteractEvent e) {
+        Player p = e.getPlayer();
+        ItemStack item = e.getItem();
+
+        if (e.getAction() == Action.RIGHT_CLICK_AIR || e.getAction() == Action.RIGHT_CLICK_BLOCK) {
+            if (item != null) {
+                // If player is holding a book and quill
+                if (item.getType() == Material.WRITABLE_BOOK) {
+                    // If the player isn't a member of any claims
+                    if (!utils.countMemberClaims().containsKey(p.getName().toLowerCase())) {
+                        // Send the player instruction on what to do with the book
+                        p.sendMessage(utils.chat("&cNOTE: &7To make a claim book, type &a\"claim\" &7at the top of the book!"));
+                        p.sendMessage(utils.chat("&aThen write a player's ign on each line to add a member!"));
+                        p.spigot().sendMessage(new ComponentBuilder(utils.chat("&aCLICK HERE to see an example"))
+                                .event(new ClickEvent(ClickEvent.Action.OPEN_URL, "https://hasjamon.github.io/b4block/lists.html"))
+                                .create());
+                    }
+                } else if (item.getType() == Material.WRITTEN_BOOK) {
+                    BookMeta meta = (BookMeta) item.getItemMeta();
+
+                    if (meta != null && meta.getLore() != null) {
+                        // If the book is not a copy
+                        if (!meta.hasGeneration() || meta.getGeneration() == BookMeta.Generation.ORIGINAL) {
+                            item.setType(Material.WRITABLE_BOOK);
+                            item.setItemMeta(meta);
+                            item.addUnsafeEnchantment(Enchantment.LUCK, 1);
+                        } else {
+                            FileConfiguration masterBooks = plugin.cfg.getMasterBooks();
+                            String bookID = String.join("", meta.getLore()).substring(17);
+
+                            if (masterBooks.contains(bookID + ".pages")) {
+                                if (plugin.getConfig().getBoolean("enable-master-books"))
+                                    meta.setPages(masterBooks.getStringList(bookID + ".pages"));
+                                item.setItemMeta(meta);
+                                p.openBook(item);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
     @EventHandler(ignoreCancelled = true)
-    public void onInteract(PlayerInteractEvent e){
+    public void onPlaceBook(PlayerInteractEvent e){
         Player p = e.getPlayer();
         Block clickedBlock = e.getClickedBlock();
-        ItemStack item = e.getItem();
 
         if(e.getAction() == Action.RIGHT_CLICK_BLOCK && clickedBlock != null && clickedBlock.getType() == Material.LECTERN) {
             Lectern lectern = (Lectern) clickedBlock.getState();
@@ -102,47 +145,11 @@ public class BookEdit implements Listener {
                     }
                 }
             }
-        }else if(e.getAction() == Action.RIGHT_CLICK_AIR || e.getAction() == Action.RIGHT_CLICK_BLOCK){
-            if(item != null) {
-                // If player is holding a book and quill
-                if (item.getType() == Material.WRITABLE_BOOK) {
-                    // If the player isn't a member of any claims
-                    if (!utils.countMemberClaims().containsKey(p.getName().toLowerCase())) {
-                        // Send the player instruction on what to do with the book
-                        p.sendMessage(utils.chat("&cNOTE: &7To make a claim book, type &a\"claim\" &7at the top of the book!"));
-                        p.sendMessage(utils.chat("&aThen write a player's ign on each line to add a member!"));
-                        p.spigot().sendMessage(new ComponentBuilder(utils.chat("&aCLICK HERE to see an example"))
-                                .event(new ClickEvent(ClickEvent.Action.OPEN_URL, "https://hasjamon.github.io/b4block/lists.html"))
-                                .create());
-                    }
-                }else if(item.getType() == Material.WRITTEN_BOOK){
-                    BookMeta meta = (BookMeta) item.getItemMeta();
-
-                    if(meta != null && meta.getLore() != null) {
-                        // If the book is not a copy
-                        if(!meta.hasGeneration() || meta.getGeneration() == BookMeta.Generation.ORIGINAL) {
-                            item.setType(Material.WRITABLE_BOOK);
-                            item.setItemMeta(meta);
-                            item.addUnsafeEnchantment(Enchantment.LUCK, 1);
-                        }else{
-                            FileConfiguration masterBooks = plugin.cfg.getMasterBooks();
-                            String bookID = String.join("", meta.getLore()).substring(17);
-
-                            if(masterBooks.contains(bookID + ".pages")) {
-                                if(plugin.getConfig().getBoolean("enable-master-books"))
-                                    meta.setPages(masterBooks.getStringList(bookID + ".pages"));
-                                item.setItemMeta(meta);
-                                p.openBook(item);
-                            }
-                        }
-                    }
-                }
-            }
         }
     }
 
     @EventHandler
-    public void onBookSave(PlayerEditBookEvent e) {
+    public void onSaveBook(PlayerEditBookEvent e) {
         Player p = e.getPlayer();
         FileConfiguration masterBooks = plugin.cfg.getMasterBooks();
         BookMeta meta = e.getNewBookMeta();

src/main/java/hasjamon/block4block/Block4Block.java
@@ -143,7 +143,7 @@ public class Block4Block extends JavaPlugin{
         if(this.getConfig().getBoolean("enable-disguises"))
             pluginManager.registerEvents(new EquipPlayerHead(this), this);
         if(this.getConfig().getBoolean("enable-claim-maps"))
-            pluginManager.registerEvents(new LecternRightClick(this), this);
+            pluginManager.registerEvents(new MapUseOnLectern(this), this);
         pluginManager.registerEvents(new MapCraft(), this);
         pluginManager.registerEvents(new PlayerInteract(), this);
         pluginManager.registerEvents(new EntityChangeBlock(this), this);

src/main/java/hasjamon/block4block/listener/MapUseOnLectern.java
@@ -20,15 +20,15 @@ import org.bukkit.map.MapView;
 import java.util.ArrayList;
 import java.util.List;
 
-public class LecternRightClick implements Listener {
+public class MapUseOnLectern implements Listener {
     private final Block4Block plugin;
 
-    public LecternRightClick(Block4Block plugin){
+    public MapUseOnLectern(Block4Block plugin){
         this.plugin = plugin;
     }
 
     @EventHandler(ignoreCancelled = true)
-    public void onPlayerInteract(PlayerInteractEvent e){
+    public void onUseMapOnLectern(PlayerInteractEvent e){
         if(e.getAction() == Action.RIGHT_CLICK_BLOCK){
             Block b = e.getClickedBlock();
 

commit 3fb7519eff46715bf3f2b683230e8e8b1d36df92

Fixed bug introduced in commit 48efade0311a7ecd80a0afef27988d92a7dfbc67 that caused copies of books to be opened and corrupted when placed on a lectern.

src/main/java/hasjamon/block4block/listener/BookEdit.java
@@ -32,38 +32,41 @@ public class BookEdit implements Listener {
     public void onOpenBook(PlayerInteractEvent e) {
         Player p = e.getPlayer();
         ItemStack item = e.getItem();
+        Block clickedBlock = e.getClickedBlock();
 
         if (e.getAction() == Action.RIGHT_CLICK_AIR || e.getAction() == Action.RIGHT_CLICK_BLOCK) {
-            if (item != null) {
-                // If player is holding a book and quill
-                if (item.getType() == Material.WRITABLE_BOOK) {
-                    // If the player isn't a member of any claims
-                    if (!utils.countMemberClaims().containsKey(p.getName().toLowerCase())) {
-                        // Send the player instruction on what to do with the book
-                        p.sendMessage(utils.chat("&cNOTE: &7To make a claim book, type &a\"claim\" &7at the top of the book!"));
-                        p.sendMessage(utils.chat("&aThen write a player's ign on each line to add a member!"));
-                        p.spigot().sendMessage(new ComponentBuilder(utils.chat("&aCLICK HERE to see an example"))
-                                .event(new ClickEvent(ClickEvent.Action.OPEN_URL, "https://hasjamon.github.io/b4block/lists.html"))
-                                .create());
-                    }
-                } else if (item.getType() == Material.WRITTEN_BOOK) {
-                    BookMeta meta = (BookMeta) item.getItemMeta();
+            if(clickedBlock == null || clickedBlock.getType() != Material.LECTERN) {
+                if (item != null) {
+                    // If player is holding a book and quill
+                    if (item.getType() == Material.WRITABLE_BOOK) {
+                        // If the player isn't a member of any claims
+                        if (!utils.countMemberClaims().containsKey(p.getName().toLowerCase())) {
+                            // Send the player instruction on what to do with the book
+                            p.sendMessage(utils.chat("&cNOTE: &7To make a claim book, type &a\"claim\" &7at the top of the book!"));
+                            p.sendMessage(utils.chat("&aThen write a player's ign on each line to add a member!"));
+                            p.spigot().sendMessage(new ComponentBuilder(utils.chat("&aCLICK HERE to see an example"))
+                                    .event(new ClickEvent(ClickEvent.Action.OPEN_URL, "https://hasjamon.github.io/b4block/lists.html"))
+                                    .create());
+                        }
+                    } else if (item.getType() == Material.WRITTEN_BOOK) {
+                        BookMeta meta = (BookMeta) item.getItemMeta();
 
-                    if (meta != null && meta.getLore() != null) {
-                        // If the book is not a copy
-                        if (!meta.hasGeneration() || meta.getGeneration() == BookMeta.Generation.ORIGINAL) {
-                            item.setType(Material.WRITABLE_BOOK);
-                            item.setItemMeta(meta);
-                            item.addUnsafeEnchantment(Enchantment.LUCK, 1);
-                        } else {
-                            FileConfiguration masterBooks = plugin.cfg.getMasterBooks();
-                            String bookID = String.join("", meta.getLore()).substring(17);
-
-                            if (masterBooks.contains(bookID + ".pages")) {
-                                if (plugin.getConfig().getBoolean("enable-master-books"))
-                                    meta.setPages(masterBooks.getStringList(bookID + ".pages"));
+                        if (meta != null && meta.getLore() != null) {
+                            // If the book is not a copy
+                            if (!meta.hasGeneration() || meta.getGeneration() == BookMeta.Generation.ORIGINAL) {
+                                item.setType(Material.WRITABLE_BOOK);
                                 item.setItemMeta(meta);
-                                p.openBook(item);
+                                item.addUnsafeEnchantment(Enchantment.LUCK, 1);
+                            } else {
+                                FileConfiguration masterBooks = plugin.cfg.getMasterBooks();
+                                String bookID = String.join("", meta.getLore()).substring(17);
+
+                                if (masterBooks.contains(bookID + ".pages")) {
+                                    if (plugin.getConfig().getBoolean("enable-master-books"))
+                                        meta.setPages(masterBooks.getStringList(bookID + ".pages"));
+                                    item.setItemMeta(meta);
+                                    p.openBook(item);
+                                }
                             }
                         }
                     }