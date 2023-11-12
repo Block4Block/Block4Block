6b4fa7f41f137407134adbcd6ada56ecffddbe8b
Bahm <bahm@users.noreply.github.com>
2023-11-12 20:58:02 +0700

    Prevent lava spread at Y > 72.

--- src/main/java/hasjamon/block4block/listener/BlockSpread.java
+++ src/main/java/hasjamon/block4block/listener/BlockSpread.java
@@ -1,28 +1,52 @@
 package hasjamon.block4block.listener;
 
 import hasjamon.block4block.utils.utils;
-import org.bukkit.Bukkit;
-import org.bukkit.ChatColor;
-import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockFromToEvent;
 import org.bukkit.event.block.BlockSpreadEvent;
 
 public class BlockSpread implements Listener {
+    private static final int Y_MAX_LAVA_PLACEMENT = 72;
+
+    @EventHandler(priority = EventPriority.LOWEST, ignoreCancelled = true)
+    public void onBlockFromTo(BlockFromToEvent event) {
+        Block source = event.getBlock();
+        Block block = event.getToBlock();
+
+        boolean shouldCancel = shouldCancelEvent(source, block);
+
+        event.setCancelled(shouldCancel);
+    }
+
     @EventHandler(priority = EventPriority.LOWEST, ignoreCancelled = true)
     public void onBlockSpread(BlockSpreadEvent event) {
-        Material sourceType = event.getSource().getType();
+        Block source = event.getSource();
+        Block block = event.getBlock();
 
-        if (sourceType == Material.SCULK_CATALYST) {
-            Block block = event.getBlock();
-            String claimID = utils.getClaimID(block.getLocation());
-            String[] members = utils.getMembers(claimID);
+        boolean shouldCancel = shouldCancelEvent(source, block);
 
-            if (members != null) {
-                event.setCancelled(true);
-            }
+        event.setCancelled(shouldCancel);
+    }
+
+    private boolean shouldCancelEvent(Block source, Block block) {
+        boolean result = false;
+
+        switch (source.getType()) {
+            case SCULK_CATALYST:
+                String claimID = utils.getClaimID(block.getLocation());
+                String[] members = utils.getMembers(claimID);
+
+                result = members != null;
+                break;
+
+            case LAVA:
+                result = source.getLocation().getBlockY() > Y_MAX_LAVA_PLACEMENT;
+                break;
         }
+
+        return result;
     }
 }
