64af16f141b7505cd6cf75a6ec36e53dc754ca6c
Bahm <bahm@users.noreply.github.com>
2023-11-19 14:02:25 +0700

    Make spawners mineable and prevent placing certain blocks inside/outside the Nether

--- src/main/java/hasjamon/block4block/listener/BlockPlace.java
+++ src/main/java/hasjamon/block4block/listener/BlockPlace.java
@@ -1,54 +1,126 @@
 package hasjamon.block4block.listener;
 
+import com.google.common.collect.Maps;
+import com.google.common.collect.Sets;
 import hasjamon.block4block.Block4Block;
 import hasjamon.block4block.events.BlockPlaceInClaimEvent;
 import hasjamon.block4block.utils.GracePeriod;
 import hasjamon.block4block.utils.utils;
 import org.bukkit.GameMode;
 import org.bukkit.Material;
+import org.bukkit.NamespacedKey;
+import org.bukkit.World;
 import org.bukkit.block.Block;
+import org.bukkit.block.CreatureSpawner;
+import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockPlaceEvent;
 import org.bukkit.event.player.PlayerBucketEmptyEvent;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.persistence.PersistentDataContainer;
+import org.bukkit.persistence.PersistentDataType;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Optional;
 
 public class BlockPlace implements Listener {
     private final Block4Block plugin;
+    private final HashMap<EntityType, EntityType> netherSpawnTypeTransformations = Maps.newHashMap();
+    private final HashMap<EntityType, EntityType> nonNetherSpawnTypeTransformations = Maps.newHashMap();
+    private final HashSet<EntityType> disallowedSpawnersOutsideNether = Sets.newHashSet(
+            EntityType.BLAZE
+    );
 
     public BlockPlace(Block4Block plugin){
+        netherSpawnTypeTransformations.put(EntityType.SLIME, EntityType.MAGMA_CUBE);
+        nonNetherSpawnTypeTransformations.put(EntityType.MAGMA_CUBE, EntityType.SLIME);
+
         this.plugin = plugin;
     }
 
     // Prevent blocks from being placed in someone else's claim
-    @EventHandler(priority = EventPriority.LOWEST)
-    public void onPlace(BlockPlaceEvent e) {
-        Block b = e.getBlock();
-        Player p = e.getPlayer();
+    @EventHandler(priority = EventPriority.LOWEST, ignoreCancelled = true)
+    public void onPlace(BlockPlaceEvent event) {
+        Block block = event.getBlockPlaced();
+        Player player = event.getPlayer();
+        Material blockType = block.getType();
+        ItemMeta itemMeta = event.getItemInHand().getItemMeta();
 
-        if (p.getGameMode() == GameMode.CREATIVE) return;
+        if (player.getGameMode() == GameMode.CREATIVE) return;
 
         // If the block was placed in a claimed chunk
-        if (plugin.cfg.getClaimData().contains(utils.getClaimID(e.getBlockPlaced().getLocation()))) {
-            String[] members = utils.getMembers(b.getLocation());
+        if (plugin.cfg.getClaimData().contains(utils.getClaimID(event.getBlockPlaced().getLocation()))) {
+            String[] members = utils.getMembers(block.getLocation());
 
             // If the block isn't the lectern claiming the chunk and the player isn't placing an easily breakable crop
-            if (!utils.isClaimBlock(b) && !isEasilyBreakableCrop(b.getType())){
+            if (!utils.isClaimBlock(block) && !isEasilyBreakableCrop(block.getType())){
                 // If the player placing the block isn't a member: Prevent block placement
-                if (members == null || !utils.isMemberOfClaim(members, p)) {
-                    plugin.pluginManager.callEvent(new BlockPlaceInClaimEvent(p, b, false));
-                    e.setCancelled(true);
-                    p.sendMessage(utils.chat("&cYou cannot place blocks in this claim"));
+                if (!utils.isMemberOfClaim(members, player)) {
+                    plugin.pluginManager.callEvent(new BlockPlaceInClaimEvent(player, block, false));
+                    event.setCancelled(true);
+                    player.sendMessage(utils.chat("&cYou cannot place blocks in this claim"));
                     return;
                 }
             }
         }
 
-        switch(e.getBlockReplacedState().getType()){
-            case AIR, CAVE_AIR, VOID_AIR, WATER, LAVA, GRASS, TALL_GRASS:
-                utils.b4bGracePeriods.put(b, new GracePeriod(System.nanoTime(), b.getType()));
+        if (block.getWorld().getEnvironment() == World.Environment.NETHER) {
+            if (blockType == Material.SPAWNER) {
+                CreatureSpawner spawner = (CreatureSpawner) block.getState();
+                Optional<EntityType> spawnTypeOpt = getSpawnerType(itemMeta);
+
+                spawnTypeOpt.ifPresent(spawnType -> transformSpawnerType(spawner, spawnType, netherSpawnTypeTransformations));
+            }
+        } else {
+            switch (blockType) {
+                case CRIMSON_NYLIUM, WARPED_NYLIUM:
+                    event.setCancelled(true);
+                    player.sendMessage(utils.chat("&cYou cannot place " + blockType.name() + " outside the Nether"));
+                    return;
+
+                case SPAWNER:
+                    CreatureSpawner spawner = (CreatureSpawner) block.getState();
+                    Optional<EntityType> spawnTypeOpt = getSpawnerType(itemMeta);
+
+                    if(spawnTypeOpt.isPresent()){
+                        EntityType spawnType = spawnTypeOpt.get();
+
+                        if (disallowedSpawnersOutsideNether.contains(spawnType)){
+                            event.setCancelled(true);
+                            player.sendMessage(utils.chat("&cYou cannot place " + utils.prettifyEnumName(spawnType) + " Spawner outside the Nether"));
+                            return;
+                        } else {
+                            transformSpawnerType(spawner, spawnType, nonNetherSpawnTypeTransformations);
+                        }
+                    }
+            }
         }
+
+        switch(event.getBlockReplacedState().getType()){
+            case AIR, CAVE_AIR, VOID_AIR, WATER, LAVA, GRASS, TALL_GRASS:
+                utils.b4bGracePeriods.put(block, new GracePeriod(System.nanoTime(), block.getType()));
+        }
+    }
+
+    private void transformSpawnerType(CreatureSpawner spawner, EntityType spawnType, HashMap<EntityType, EntityType> transforms) {
+        spawner.setSpawnedType(transforms.getOrDefault(spawnType, spawnType));
+        spawner.update();
+    }
+
+    private Optional<EntityType> getSpawnerType(ItemMeta itemMeta) {
+        if (itemMeta != null) {
+            PersistentDataContainer container = itemMeta.getPersistentDataContainer();
+            if (container.has(new NamespacedKey(plugin, "spawnType"), PersistentDataType.STRING)) {
+                String spawnTypeName = container.get(new NamespacedKey(plugin, "spawnType"), PersistentDataType.STRING);
+                return Optional.of(EntityType.valueOf(spawnTypeName));
+            }
+        }
+
+        return Optional.empty();
     }
 
     @EventHandler
