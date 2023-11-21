d2bd21162c002aa2f796ac02cc51e244eded47f8
Bahm <bahm@users.noreply.github.com>
2023-11-21 20:32:50 +0700

    Creepers now drop gunpowder when they explode

--- src/main/java/hasjamon/block4block/listener/EntityExplode.java
+++ src/main/java/hasjamon/block4block/listener/EntityExplode.java
@@ -2,6 +2,7 @@ package hasjamon.block4block.listener;
 
 import hasjamon.block4block.Block4Block;
 import hasjamon.block4block.utils.utils;
+import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.World;
 import org.bukkit.block.Block;
@@ -11,8 +12,10 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityExplodeEvent;
+import org.bukkit.inventory.ItemStack;
 
 import java.util.List;
+import java.util.Optional;
 
 public class EntityExplode implements Listener {
     private final Block4Block plugin;
@@ -32,16 +35,17 @@ public class EntityExplode implements Listener {
         };
 
         List<?> claimImmunity = plugin.getConfig().getList("claim-explosion-immunity." + dimension);
+        EntityType entityType = e.getEntityType();
 
         // If claims in the current dimension are immune to explosions from the entity
-        if(claimImmunity != null && claimImmunity.contains(e.getEntityType().toString())){
+        if(claimImmunity != null && claimImmunity.contains(entityType.toString())){
             FileConfiguration claimData = plugin.cfg.getClaimData();
 
             // Remove blocks from the to-be-exploded list if they're inside a claim
             e.blockList().removeIf(b -> claimData.contains(utils.getClaimID(b.getLocation())));
         }
 
-        if(e.getEntityType() == EntityType.PRIMED_TNT || e.getEntityType() == EntityType.MINECART_TNT) {
+        if(entityType == EntityType.PRIMED_TNT || entityType == EntityType.MINECART_TNT) {
             List<String> tntDropsEnabled = plugin.getConfig().getStringList("tnt-drops-enabled");
 
             for (Block block : e.blockList()) {
@@ -49,6 +53,13 @@ public class EntityExplode implements Listener {
                     block.setType(Material.AIR);
                 }
             }
+        }else if(entityType == EntityType.CREEPER){
+            Location location = e.getLocation();
+            Optional<World> worldOpt = Optional.ofNullable(location.getWorld());
+
+            worldOpt.ifPresent(world ->
+                    world.dropItemNaturally(location, new ItemStack(Material.GUNPOWDER))
+            );
         }
     }
 }
