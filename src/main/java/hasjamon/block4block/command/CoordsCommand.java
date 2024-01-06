9668cf4dff7d11df954f003cbe091f51325589c5
Bahm <bahm@users.noreply.github.com>
2024-01-06 20:33:57 +0700

    Coordinates in boss bar are now instantly updated when using /coords [on|off] command

--- src/main/java/hasjamon/block4block/command/CoordsCommand.java
+++ src/main/java/hasjamon/block4block/command/CoordsCommand.java
@@ -1,6 +1,7 @@
 package hasjamon.block4block.command;
 
 import hasjamon.block4block.Block4Block;
+import hasjamon.block4block.utils.utils;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
@@ -11,38 +12,37 @@ import org.bukkit.entity.Player;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 
 public class CoordsCommand implements CommandExecutor, TabCompleter {
     private final Block4Block plugin;
+    private final Set<String> validArgs = Set.of("off", "on");
 
     public CoordsCommand(Block4Block plugin) {
         this.plugin = plugin;
     }
 
-    public boolean onCommand(CommandSender sender, Command command, String label, String[] args){
-        if(sender instanceof Player && args.length > 0) {
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (sender instanceof Player player && args.length > 0) {
             FileConfiguration coordsSettings = plugin.cfg.getCoordsSettings();
-            String pUUID = ((Player) sender).getUniqueId().toString();
+            String pUUID = player.getUniqueId().toString();
+            String arg = args[0];
 
-            if(args[0].equalsIgnoreCase("off")) {
-                coordsSettings.set(pUUID, "off");
+            if (validArgs.contains(arg)) {
+                coordsSettings.set(pUUID, arg.toLowerCase());
                 plugin.cfg.saveCoordsSettings();
-                sender.sendMessage("Coords in system messages are now " + ChatColor.RED + "OFF");
-                return true;
-            }else if(args[0].equalsIgnoreCase("on")) {
-                coordsSettings.set(pUUID, "on");
-                plugin.cfg.saveCoordsSettings();
-                sender.sendMessage("Coords in system messages are now " + ChatColor.GREEN + "ON");
+                player.sendMessage("Coords in system messages are now " + ChatColor.RED + arg.toUpperCase());
+                utils.updateBossBar(player, utils.getClaimID(player.getLocation()));
                 return true;
             }
         }
         return false;
     }
 
-    public List<String> onTabComplete(CommandSender sender, Command cmd, String alias, String[] args){
+    public List<String> onTabComplete(CommandSender sender, Command cmd, String alias, String[] args) {
         List<String> suggestions = new ArrayList<>();
 
-        if(args.length == 1){
+        if (args.length == 1) {
             suggestions.add("on");
             suggestions.add("off");
         }
