877c33103ed8b046ab4e91e743aaa2eee7374f70
Bahm <bahm@users.noreply.github.com>
2024-01-10 19:20:31 +0700

    Named mobs will no longer despawn, even if the name comes from an anvil-renamed spawn egg

--- src/main/java/hasjamon/block4block/listener/CreatureSpawn.java
+++ src/main/java/hasjamon/block4block/listener/CreatureSpawn.java
@@ -3,15 +3,21 @@ package hasjamon.block4block.listener;
 import hasjamon.block4block.utils.utils;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.IronGolem;
+import org.bukkit.entity.LivingEntity;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.CreatureSpawnEvent;
 
 public class CreatureSpawn implements Listener {
     @EventHandler
-    public void onCreatureSpawn(CreatureSpawnEvent e){
-        if(e.getEntityType() == EntityType.IRON_GOLEM) {
-            IronGolem golem = (IronGolem) e.getEntity();
+    public void onCreatureSpawn(CreatureSpawnEvent e) {
+        LivingEntity entity = e.getEntity();
+        boolean isNamed = entity.getCustomName() != null;
+
+        entity.setRemoveWhenFarAway(!isNamed);
+
+        if (e.getEntityType() == EntityType.IRON_GOLEM) {
+            IronGolem golem = (IronGolem) entity;
             String chunkID = utils.getChunkID(golem.getLocation());
 
             // Make sure it can attack players
