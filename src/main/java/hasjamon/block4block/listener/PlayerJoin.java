792890278e9c18e2903a5b7548db9af02485ea56
Bahm <bahm@users.noreply.github.com>
2024-03-10 07:32:49 +0700

    Fix PlayerJoin error

--- src/main/java/hasjamon/block4block/listener/PlayerJoin.java
+++ src/main/java/hasjamon/block4block/listener/PlayerJoin.java
@@ -4,12 +4,8 @@ import com.mojang.authlib.properties.Property;
 import hasjamon.block4block.Block4Block;
 import hasjamon.block4block.events.ClaimLostWhileOfflineEvent;
 import hasjamon.block4block.utils.utils;
-import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.World;
-import org.bukkit.boss.BarColor;
-import org.bukkit.boss.BarStyle;
-import org.bukkit.boss.BossBar;
 import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
@@ -25,7 +21,7 @@ import java.util.Set;
 public class PlayerJoin implements Listener {
     private final Block4Block plugin;
 
-    public PlayerJoin(Block4Block plugin){
+    public PlayerJoin(Block4Block plugin) {
         this.plugin = plugin;
     }
 
@@ -33,17 +29,17 @@ public class PlayerJoin implements Listener {
     public void onPlayerJoin(PlayerJoinEvent e) {
         Player p = e.getPlayer();
 
-        if(!p.hasPlayedBefore()) {
+        if (!p.hasPlayedBefore()) {
             utils.sendWelcomeMsg(p);
         }
-        
+
         utils.knownPlayers.add(p.getName().toLowerCase());
         utils.populatePlayerClaimsIntruded(p);
         utils.updateClaimCount();
 
         String claimID = utils.getClaimID(p.getLocation());
         utils.updateBossBar(p, claimID);
-        if(utils.isIntruder(p, claimID))
+        if (utils.isIntruder(p, claimID))
             utils.onIntruderEnterClaim(p, claimID);
 
 
@@ -52,21 +48,21 @@ public class PlayerJoin implements Listener {
         ConfigurationSection claimsLost = offlineClaimNotifications.getConfigurationSection(pName + ".chunks");
         ConfigurationSection masterBooksRemovedFrom = offlineClaimNotifications.getConfigurationSection(pName + ".masterbooks");
 
-        if(claimsLost != null){
+        if (claimsLost != null) {
             Set<String> claimIDs = claimsLost.getKeys(false);
             int i = 0;
 
-            for(String cID : claimIDs){
-                if(++i >= 10 && claimIDs.size() > 10){
+            for (String cID : claimIDs) {
+                if (++i >= 10 && claimIDs.size() > 10) {
                     p.sendMessage(ChatColor.RED + "... and " + (claimIDs.size() - 9) + " other claims");
                     break;
                 }
 
                 String xyz = claimsLost.getString(cID);
                 String worldName = utils.getWorldName(World.Environment.valueOf(claimID.split("\\|")[0]));
-                if(!plugin.getConfig().getBoolean("hide-coords-globally") && utils.showCoordsInMsgs(p)) {
+                if (!plugin.getConfig().getBoolean("hide-coords-globally") && utils.showCoordsInMsgs(p)) {
                     p.sendMessage(ChatColor.RED + "You have lost a claim! Location: " + xyz + " in " + worldName);
-                }else{
+                } else {
                     p.sendMessage(ChatColor.RED + "You have lost a claim! Location: [hidden] in " + worldName);
                 }
             }
@@ -74,8 +70,8 @@ public class PlayerJoin implements Listener {
             plugin.pluginManager.callEvent(new ClaimLostWhileOfflineEvent(p));
         }
 
-        if(masterBooksRemovedFrom != null){
-            for(String mbID : masterBooksRemovedFrom.getKeys(false)) {
+        if (masterBooksRemovedFrom != null) {
+            for (String mbID : masterBooksRemovedFrom.getKeys(false)) {
                 if (masterBooksRemovedFrom.getBoolean(mbID)) {
                     p.sendMessage(ChatColor.RED + "Your name has been removed from Master Book #" + mbID + " and all related claims!");
                     plugin.pluginManager.callEvent(new ClaimLostWhileOfflineEvent(p));
@@ -88,13 +84,13 @@ public class PlayerJoin implements Listener {
 
         Collection<Property> textures = utils.getTextures(p);
 
-        if(textures != null){
+        if (textures != null && !textures.isEmpty()) {
             Property prop = textures.iterator().next();
             List<String> copy = new ArrayList<>();
 
             copy.add(prop.getName());
             copy.add(prop.getValue());
-            if(prop.hasSignature())
+            if (prop.hasSignature())
                 copy.add(prop.getSignature());
 
             plugin.cfg.getPlayerTextures().set(p.getUniqueId().toString(), copy);
